585d3893172aa514d404f09f3462a1238523a988
siennalind <sienna.lebeaulind@gmail.com>
2022-10-05 23:42:07 +0100

    add: score & checking if answer is correct

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -3,30 +3,54 @@ import { useState } from 'react'
 
 function Question () {
     const [currentQuestion, setCurrentQuestion] = useState(0)
+    const [score, setScore] = useState(0)
+    const [isCorrect, setIsCorrect] = useState(false)
 
     const questions = DUMMY_DATA
 
-    const handleNext = (e) => {
-        e.preventDefault()
+    const handleNext = () => {
         const nextIndex = currentQuestion + 1
         setCurrentQuestion(nextIndex)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        let newScore = 0
+
+        console.log(isCorrect)
+
+        if (isCorrect) {
+            newScore = score + 1
+            setScore(newScore)
+        }
+
+        console.log(score)
+    }
+
+    const handleSelect = (answer) => {
+        setIsCorrect(answer.isTrue)
     }
 
     return (
         <div>
         <p>{questions[currentQuestion].questionText}</p>
-        <form>
+        <form key={questions[currentQuestion].questionText}>
         {questions[currentQuestion].options.map((option) => (
             <>
             <input 
             type="radio" 
             name={questions[currentQuestion]} 
-            value={option.text} />
+            value={option.text} 
+            onClick={() => {handleSelect(option)}} />
             <label>{option.text}</label>
             </>
         ))}
-        <input type="submit" onClick={handleNext} value="Next" />
+        <input 
+        onClick={handleSubmit} 
+        type="submit" 
+        value="Submit" />
         </form>
+        <button onClick={handleNext}>Next</button>
         </div>
     )
 }
