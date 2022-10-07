f7530ce4d3972ae700e951620ee5e2b767be9dfa
siennalind <sienna.lebeaulind@gmail.com>
2022-10-07 17:55:00 +0100

    add: score tracking, feedback, form disabled on submit

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -5,26 +5,31 @@ function Question () {
     const [currentQuestion, setCurrentQuestion] = useState(0)
     const [score, setScore] = useState(0)
     const [isCorrect, setIsCorrect] = useState(false)
+    const [submitted, setSubmitted] = useState(false)
+    const [feedback, setFeedback] = useState('')
 
     const questions = DUMMY_DATA
-
-    const handleNext = () => {
-        const nextIndex = currentQuestion + 1
-        setCurrentQuestion(nextIndex)
-    }
 
     const handleSubmit = (e) => {
         e.preventDefault()
         let newScore = 0
 
-        console.log(isCorrect)
-
         if (isCorrect) {
             newScore = score + 1
             setScore(newScore)
+            setFeedback('You were right!')
+        } else {
+            setFeedback(`You were incorrect`)
         }
 
-        console.log(score)
+        setSubmitted(true)
+    }
+
+    const handleNext = () => {
+        const nextIndex = currentQuestion + 1
+        setCurrentQuestion(nextIndex)
+        setSubmitted(false)
+        setFeedback('')
     }
 
     const handleSelect = (answer) => {
@@ -41,15 +46,20 @@ function Question () {
             type="radio" 
             name={questions[currentQuestion]} 
             value={option.text} 
-            onClick={() => {handleSelect(option)}} />
+            onClick={() => {handleSelect(option)}} 
+            />
             <label>{option.text}</label>
             </>
         ))}
         <input 
         onClick={handleSubmit} 
         type="submit" 
-        value="Submit" />
+        value="Submit" 
+        disabled={submitted}
+        />
         </form>
+        <p>Your score is {score}</p>
+        <p>{feedback}</p>
         <button onClick={handleNext}>Next</button>
         </div>
     )
