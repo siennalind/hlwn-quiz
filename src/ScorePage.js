22d8fd8179ef7804da59cf4b35fa90f163508c57
siennalind <sienna.lebeaulind@gmail.com>
2022-10-12 18:49:08 +0100

    add: score & start pages

---
+++ src/ScorePage.js
@@ -0,0 +1,10 @@
+function ScorePage () {
+    return (
+        <div>
+            <p>Your score was x out of x</p>
+            <button>Retry?</button>
+        </div>
+    )
+}
+
+export default ScorePage
---
+++ src/StartPage.js
@@ -0,0 +1,10 @@
+function StartPage () {
+    return (
+        <>
+            <p>Hello World</p>
+            <button>Start Quiz</button>
+        </>
+    )
+}
+
+export default StartPage
