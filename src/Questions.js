35dee81be3f1da4eb025380e2710be55ce76c9f9
siennalind <sienna.lebeaulind@gmail.com>
2022-10-29 18:23:10 +0100

    git add: questions

---
+++ src/Questions.js
@@ -0,0 +1,156 @@
+const Questions = [
+    {
+        questionText: "Which item can ward off a vampire?",
+        id: 1,
+        options: [
+            {
+                text: "Tomato",
+                isTrue: false
+            }, 
+            {
+                text: "Rosemary",
+                isTrue: false
+            }, 
+            {
+                text: "Garlic",
+                isTrue: true
+            }, 
+            {
+                text: "Sage",
+                isTrue: false
+            }
+        ]
+    },
+    {
+        questionText: "What is the name of the friendly ghost?",
+        id: 2,
+        options: [
+            {
+                text: "Casper",
+                isTrue: true
+            }, 
+            {
+                text: "Mary",
+                isTrue: false
+            }, 
+            {
+                text: "Alan",
+                isTrue: false
+            }, 
+            {
+                text: "Cassandra",
+                isTrue: false
+            }
+        ]
+    },
+    {
+        questionText: "In the film 'Nightmare before Christmas', what town did Jack Skellington live in?",
+        id: 3,
+        options: [
+            {
+                text: "Christmas Town",
+                isTrue: false
+            }, 
+            {
+                text: "Easter Town",
+                isTrue: false
+            }, 
+            {
+                text: "Midsummer Town",
+                isTrue: false
+            }, 
+            {
+                text: "Halloween Town",
+                isTrue: true
+            }
+        ]
+    },
+    {
+        questionText: "What type of fruit is often carved to make Jack-o'-Lanterns nowadays?",
+        id: 4,
+        options: [
+            {
+                text: "Pumpkin",
+                isTrue: true
+            }, 
+            {
+                text: "Orange",
+                isTrue: false
+            }, 
+            {
+                text: "Apple",
+                isTrue: false
+            }, 
+            {
+                text: "Kiwi",
+                isTrue: false
+            }
+        ]
+    },
+    {
+        questionText: "Bobbing for ___",
+        id: 5,
+        options: [
+            {
+                text: "Strawberries",
+                isTrue: false
+            }, 
+            {
+                text: "Coconuts",
+                isTrue: false
+            }, 
+            {
+                text: "Apples",
+                isTrue: true
+            }, 
+            {
+                text: "Hats",
+                isTrue: false
+            }
+        ]
+    },
+    {
+        questionText: "What tool are witches often depicted riding on?",
+        id: 7,
+        options: [
+            {
+                text: "Shovel",
+                isTrue: false
+            }, 
+            {
+                text: "Broom",
+                isTrue: true
+            }, 
+            {
+                text: "Leafblower",
+                isTrue: false
+            }, 
+            {
+                text: "Rake",
+                isTrue: false
+            }
+        ]
+    },
+    {
+        questionText: "During which phase of the moon would someone turn into a werewolf?",
+        id: 8,
+        options: [
+            {
+                text: "New moon",
+                isTrue: false
+            }, 
+            {
+                text: "Waxing crescent",
+                isTrue: false
+            }, 
+            {
+                text: "Waning gibbous",
+                isTrue: false
+            }, 
+            {
+                text: "Full moon",
+                isTrue: true
+            }
+        ]
+    }
+]
